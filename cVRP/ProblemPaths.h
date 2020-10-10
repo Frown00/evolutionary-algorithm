96e05d79008c494e74bd7da92ab14520e06ed2c5
Frown00 <fabian.kuriata.dev@gmail.com>
2020-10-10 20:03:51 +0200

    feat: add all problems' paths

---
+++ cVRP/ProblemPaths.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+const std::string PROBLEM_A_N32_K5 = "./problems/A-n32-k5.vrp";
+const std::string PROBLEM_A_N37_K6 = "./problems/A-n37-k6.vrp";
+const std::string PROBLEM_A_N39_K5 = "./problems/A-n39-k5.vrp";
+const std::string PROBLEM_A_N45_K6 = "./problems/A-n45-k6.vrp";
+const std::string PROBLEM_A_N48_K7 = "./problems/A-n48-k7.vrp";
+const std::string PROBLEM_A_N54_K7 = "./problems/A-n54-k7.vrp";
+const std::string PROBLEM_A_N60_K9 = "./problems/A-n60-k9.vrp";
