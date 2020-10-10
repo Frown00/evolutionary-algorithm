43b1c3031dec526f800841e1c2a12ca428197a56
Frown00 <fabian.kuriata.dev@gmail.com>
2020-10-10 20:06:15 +0200

    feat(config): add all cVRP parameters with some default values

---
+++ cVRP/config.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include "ProblemPaths.h"
+namespace config {
+	// path to file with vehicle routing problem
+	const std::string FILENAME = PROBLEM_A_N32_K5;
+	
+	// population size
+	const int POP_SIZE = 100;
+	
+	// generations amount
+	const int GEN = 100;
+	
+	// percent chance for crossing
+	const double P_X = 70;
+	
+	// percent chance for mutation
+	const double P_M = 10;
+
+	// used for tournament selection
+	// number of individuals participating in one tournament
+	const double TOUR = 5;													
+};
