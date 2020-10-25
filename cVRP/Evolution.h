bb7173b9f45a27bae97f20f2818c902b9a550416
Frown00 <fabian.kuriata.dev@gmail.com>
2020-10-25 20:01:32 +0100

    feat: swap and inversion mutation

--- cVRP/Evolution.h
+++ cVRP/Evolution.h
@@ -3,16 +3,30 @@
 #include "Individual.h"
 #include "Location.h"
 #include "VehicleRoutingProblem.h"
-#include "Config.h"
 
-class Evolution {
+namespace evolution {
+	const enum class MutationType {
+		SWAP = 0,
+		INVERSION = 1,
+	};
+
+	const enum class CrossoverType {
+		ORDERED = 0,
+		PARTIALLY_MATCHED = 1,
+		CYCLE = 2
+	};
+
+	class Evolution {
 	public:
 		Evolution(VehicleRoutingProblem* t_problem);
-		void createPopulation();
-
+		void solve();
 	private:
+		void createPopulation();
+		void mutation();
+		void crossover();
 		VehicleRoutingProblem* m_problem;
 		std::vector<Individual*> m_population;
 
-};
+	};
+}
 
