837644745804c8c5beceace52f47fbfc69b7cdde
Frown00 <fabian.kuriata.dev@gmail.com>
2020-11-08 17:45:27 +0100

    fix: greedy and random algorithm

--- cVRP/main.cpp
+++ cVRP/main.cpp
@@ -2,6 +2,8 @@
 #include "Loader.h"
 #include "Evolution.h"
 #include <time.h>
+#include "Writer.h"
+#include "Config.h"
 
 using namespace evolution;
 
@@ -13,7 +15,8 @@ int main()
   VehicleRoutingProblem* problem = loader->getProblem();
   Summary* greedy = problem->greedySolution();
   Summary* random = problem->randomSolution(10000);
-  Evolution evolution = Evolution(problem);
-  evolution.solve(10);
-  evolution.saveResults(greedy, random);
+  Evolution* ea = new Evolution(problem);
+  std::vector<Summary*> evolution = ea->solve(10);
+  Writer* writer = new Writer();
+  writer->saveResults(greedy, random, evolution);
 }
