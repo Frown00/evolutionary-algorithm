3711714e2bc898cf04a09723adac4c7d8e59bf9b
Frown00 <fabian.kuriata.dev@gmail.com>
2020-11-08 17:44:14 +0100

    fix: evoulution algorithm structure and methods

--- cVRP/Evolution.cpp
+++ cVRP/Evolution.cpp
@@ -10,46 +10,37 @@ Evolution::Evolution(VehicleRoutingProblem* t_problem) {
 	m_problem = t_problem;
 }
 
-void Evolution::solve(int t_test_num) {
-	createPopulation();
-	std::vector<std::vector<Summary*>> test_summary;
-	for(int t = 0; t < t_test_num; t++) {
-		std::cout << "EVOLUTION TEST: " << t + 1 << "\n";
-		std::vector<Summary*> generation_summaries;
-		for (int g = 1; g <= config::GEN; g++) {
+std::vector<Summary*> Evolution::solve(int t_test_num) {
+	Experiment* experiment = new Experiment();
+	for(int t = 1; t <= t_test_num; t++) {
+		Test* test = new Test(t);
+		std::cout << "\nEVOLUTION TEST: " << t << "\n";
+		createPopulation();
+		for(int g = 1; g <= config::GEN; g++) {
+			Summary* generation_summary = new Summary(g);
 			std::cout << "\r" << "EVOLUTION PROGRESS: " << g << " / " << config::GEN;
-			Summary* summary = new Summary();
-			for (int i = 0; i < m_population.size(); i++) {
-				summary->addResult(stoi(std::to_string(g) + std::to_string(i)), m_population[i]->getFitness());
+			for(int i = 0; i < m_population.size(); i++) {
+				generation_summary->addResult(m_population[i]);
 			}
-			summary->countAverage();
-			summary->countStd();
-			generation_summaries.push_back(summary);
-			mutation();
+			generation_summary->countAverage();
+			test->addSummary(generation_summary);
 			crossover();
+			mutation();
 			refreshFitness();
 			selection();
 		}
-		std::cout << "\nCOMPLETED\n";
-		test_summary.push_back(generation_summaries);
+		std::cout << "\n\n" << "BEST PATH:";
+		test->getBestRoute()->printRouting(m_problem->getDepot());
+		experiment->addTest(test);
 	}
-	for(int g = 0; g < config::GEN; g++) {
-		Summary* overall = new Summary();
-		for(int j = 0; j < config::POP_SIZE; j++) {
-			double sum = 0;
-			for(int t = 0; t < test_summary.size(); t++) {
-				Summary* summary = test_summary[t][g];
-				sum += summary->getResult(j);
-			}
-			overall->addResult(0, sum / t_test_num);
-		}
-		overall->countAverage();
-		overall->countStd();
-		m_test_summaries.push_back(overall);
-	}
+	std::cout << "\nCOMPLETED\n";
+	return experiment->getOverallSummary();
 }
 
 void Evolution::createPopulation() {
+	/*for (std::vector<Individual*>::iterator i = m_population.begin(); i != m_population.end(); ++i)
+		delete* i;*/
+	m_population.clear();
 	for(int i = 0; i < config::POP_SIZE; i++) {
 		Individual* individual = new Individual(m_problem->getDimension());
 		individual->setRandomGenotype(
@@ -105,12 +96,15 @@ void evolution::Evolution::tournamentSelection()
 {
 	std::vector<Individual*> new_population;
 	int rivals_amount = m_population.size() * (config::TOUR / 100);
+	if (rivals_amount <= 0) {
+		rivals_amount = 1;
+	}
 	for(int i = 0; i < config::POP_SIZE; i++) {
 		std::vector<int> tournament;
 		// select rivals
 		for(int r = 0; r < rivals_amount; r++) {
 			int index = rand() % m_population.size();
-			while(utils::find_index(tournament, index) != -1) {
+			while(utils::findIndex(tournament, index) != -1) {
 				index = rand() % m_population.size();
 			}
 			tournament.push_back(index);
@@ -182,90 +176,4 @@ void evolution::Evolution::refreshFitness()
 	for(int i = 0; i < m_population.size(); i++) {
 		m_population[i]->countFitness(m_problem->getDepot(), m_problem->getLocations());
 	}
-}
-
-void evolution::Evolution::saveResults(Summary* t_greedy, Summary* t_random)
-{
-	std::ofstream myfile;
-	std::string mutation_name = "";
-	std::string mutation_type = "";
-	std::string crossover_name = "";
-	std::string crossover_type = "";
-	std::string selection_name = "";
-	std::string selection_type = "";
-
-	switch (config::MUTATION_TYPE) {
-	case evolution::MutationType::SWAP:
-		mutation_name = "S_PM";
-		mutation_type = "SWAP";
-		break;
-	case evolution::MutationType::INVERSION:
-		mutation_name = "I_PM";
-		mutation_type = "INVERSION";
-		break;
-	}
-	switch (config::CROSSOVER_TYPE) {
-	case evolution::CrossoverType::ORDERED:
-		crossover_name = "O_PX";
-		crossover_type = "ORDERED";
-		break;
-	case evolution::CrossoverType::CYCLE:
-		crossover_name = "C_PX";
-		crossover_type = "CYCLE";
-		break;
-	}
-	switch (config::SELECTION_TYPE) {
-	case evolution::SelectionType::TOURNAMENT:
-		selection_name = "T" + std::to_string(config::TOUR);
-		selection_type = "TOURNAMENT";
-		break;
-	case evolution::SelectionType::ROULETTE:
-		selection_name = "R";
-		selection_type = "ROULETTE";
-		break;
-	}
-	std::string filename = config::INSTANCE_PROBLEM + "-"
-		"POP" + std::to_string(config::POP_SIZE) + "-" +
-		"GEN" + std::to_string(config::GEN) + "-" +
-		crossover_name + std::to_string((int)config::P_X) + "-" +
-		mutation_name + std::to_string((int)config::P_M) + "-" +
-		selection_name;
-	myfile.open("./results/" + filename + ".csv");
-	myfile << "Constrained vehicle routing problem\n";
-	myfile << "Instance name: " + config::INSTANCE_PROBLEM + "\n";
-	myfile << "Evolution parameters\n";
-	myfile << "Population size:;" + std::to_string(config::POP_SIZE) + "\n";
-	myfile << "Generation number:;" + std::to_string(config::GEN) + "\n";
-	myfile << "Mutation type:;" + mutation_type + "; Probability: " + std::to_string(config::P_M) + "\n";
-	myfile << "Crossover type:;" + crossover_type + "; Probability: " + std::to_string(config::P_X) + "\n";
-	myfile << "Selection type:;" + selection_type;
-	if (config::SELECTION_TYPE == evolution::SelectionType::TOURNAMENT) {
-		myfile << ";Parameter: " + std::to_string(config::TOUR) + "\n";
-	}
-	else {
-		myfile << "\n";
-	}
-	myfile << "\nGreedy solution\n";
-	myfile << "Best;Worst;Average;Std\n";
-	myfile << std::to_string(t_greedy->getBest()) + ";" +
-		std::to_string(t_greedy->getWorst()) + ";" +
-		std::to_string(t_greedy->getAverage()) + ";" +
-		std::to_string(t_greedy->getStd()) + "\n";
-	myfile << "\nRandom solution\n";
-	myfile << "Best;Worst;Average;Std\n";
-	myfile << std::to_string(t_random->getBest()) + ";" +
-		std::to_string(t_random->getWorst()) + ";" +
-		std::to_string(t_random->getAverage()) + ";" +
-		std::to_string(t_random->getStd()) + "\n";
-	myfile << "\nEvolution solution\n";
-	myfile << "Generation;Best;Worst;Average;Std\n";
-	for(int g = 0; g < m_test_summaries.size(); g++) {
-		myfile << 
-			std::to_string(g) + ";" +
-			std::to_string(m_test_summaries[g]->getBest()) + ";" +
-			std::to_string(m_test_summaries[g]->getWorst()) + ";" +
-			std::to_string(m_test_summaries[g]->getAverage()) + ";" +
-			std::to_string(m_test_summaries[g]->getStd()) + "\n";
-	}
-	myfile.close();
 }
